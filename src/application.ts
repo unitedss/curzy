08eada03fb17fc0ec57e8287722828aac2bc77b1
Fifix <122852085+Fifixex@users.noreply.github.com>
2024-07-29 23:31:11 -0300

    :recycle: refactor: improving class structures

--- src/application.ts
+++ src/application.ts
@@ -1,41 +1,53 @@
 import { post } from './utils'
 
 export class Application {
-    private password: string = process.env.SYSTEM_PASSWORD as string
-
-    public async create(name: string) {
-        const id = await this.getId(name)
-        const ticket = await this.getTicket(id, this.password)
-        const cookie = await this.getCookie(ticket, this.password)
-        const token = await this.getToken(id, cookie)
-        return token
+    #token: string = process.env.SYSTEM_TOKEN as string
+    protected password: string = process.env.SYSTEM_PASSWORD as string
+    private headers: HeadersInit = {
+        'Content-Type': 'application/json',
+        'Authorization': this.#token
     }
 
-    private async getId(name: string) {
-        const app = await post(`/applications`, 'POST', { name })
+    protected async getId(name: string) {
+        const app = await post(`/applications`, {
+            headers: this.headers,
+            body: JSON.stringify({
+                name
+            })
+        })
+
         return app.id as string
     }
 
-    private async getTicket(id: string, password: string = this.password) {
-        const reset = await post(`/applications/${id}/bot/reset`, 'POST')
+    protected async getTicket(id: string, password: string = this.password) {
+        const reset = await post(`/applications/${id}/bot/reset`, {
+            headers: this.headers
+        })
+
         return reset.mfa.ticket as string
     }
 
-    private async getCookie(ticket: string, password: string = this.password) {
-        const body = {
-            'data': password,
-            'mfa_type': 'password',
-            'ticket': ticket
-        }
-        const cookie = await post(`/mfa/finish`, 'POST', body)
+    protected async getCookie(ticket: string, password: string = this.password) {
+        const cookie = await post(`/mfa/finish`, {
+            headers: this.headers,
+            body: JSON.stringify({
+                'data': password,
+                'mfa_type': 'password',
+                'ticket': ticket
+            })
+        })
+
         return cookie.token as string
     }
 
-    private async getToken(id: string, cookie: string) {
-        const headers = {
-            'x-discord-mfa-authorization': cookie
-        }
-        const response = await post(`/applications/${id}/bot/reset`, 'POST', headers)
-        return response.token;
+    protected async getToken(id: string, cookie: string) {
+        const response = await post(`/applications/${id}/bot/reset`, {
+            headers: {
+                ...this.headers,
+                'x-discord-mfa-authorization': cookie
+            }
+        })
+        
+        return response.token as string;
     }
 }
--- src/bot-manager.ts
+++ src/bot-manager.ts
@@ -1,19 +1,24 @@
 import { Application } from './application'
 
 export class Bot extends Application {
-    public name!: string;
+    private name!: string
+
     constructor(options: BotOptions) {
         super()
         this.validate(options)
     }
-
+    
     private validate(options: BotOptions) {
         if (typeof options !== 'object') throw TypeError('Options should be a type of Object.')
         if (!options.name) throw Error('Por favor, especifica el nombre del bot!')
         this.name = options.name
     }
-    
-    public async start(token: string) {
-        // await this.login(token as string)
+
+    public async create() {
+        const id = await this.getId(this.name)
+        const ticket = await this.getTicket(id, this.password)
+        const cookie = await this.getCookie(ticket, this.password)
+        const token = await this.getToken(id, cookie)
+        return token
     }
 }
