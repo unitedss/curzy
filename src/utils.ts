c2b46fac94fbdc7e44b4558e098e92ac54265de0
Fifix <122852085+Fifixex@users.noreply.github.com>
2024-07-29 23:31:46 -0300

    :bug: fix: fetch post function

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,9 @@
-const DISCORD_API = 'https://discord.com/api/v9'
-const SYSTEM_TOKEN = process.env.SYSTEM_TOKEN as string
-
-export const post = async (path: string, method: string, headers?: Record<string, string>, body: unknown = {}) => {
-    const response = await fetch(DISCORD_API + path, {
-        method,
-        headers: {
-            'Authorization': SYSTEM_TOKEN,
-            'Content-Type': 'application/json',
-            ...headers
-        },
-        body: JSON.stringify(body)
-    })
-    return await response.json()
+export const post = async (path: string, options: RequestInit) => {
+    options.method = 'POST'
+    const url = new URL('https://discord.com/api/v9' + path)
+    const response = await fetch(url, options)
+    const data = await response.json()
+    if (!response.ok) throw new Error (`Error: ${response.status} ${data.error.message}`)
+    
+    return data
 }
