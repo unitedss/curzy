71e02986dd89170e0dab10d107ac6b2ccab666f0
Fifix <122852085+Fifixex@users.noreply.github.com>
2024-08-01 09:52:56 -0300

    :wheelchair: refactor: improve accessibility and scalability

--- src/application.ts
+++ src/application.ts
@@ -1,50 +1,32 @@
-import { getTrackBase64, post } from './utils'
+import { Network } from './network'
 
 export class Application {
-    #token: string = process.env.SYSTEM_TOKEN as string
-    protected password: string = process.env.SYSTEM_PASSWORD as string
-    private headers: HeadersInit = {
-        'Content-Type': 'application/json',
-        'Authorization': this.#token,
-        'X-Track': getTrackBase64()
+    private rest = new Network
+    constructor() {
+        this.rest = new Network()
+    }
+    
+    protected async createApplication(name: string) {
+        const application = await this.rest.createApplication(name)
+        const ticket = await this.rest.getTicket(application.id)
+        const cookie = await this.rest.getCookie(ticket)
+        const token = await this.rest.getToken(application.id, cookie)
+
+        return {
+            ...application,
+            token
+        }
     }
 
-    protected async getId(name: string) {
-        const app = await post(`/applications`, {
-            headers: this.headers,
-            body: JSON.stringify({
-                name
-            })
-        })
-        return app.id as string
+    protected async getApplication(id: string) {
+        if (typeof id !== 'string' || !id) throw Error('ID not valid!')
+        const response = await this.rest.getApplication(id)
+        return response
     }
-
-    protected async getTicket(id: string, password: string = this.password) {
-        const reset = await post(`/applications/${id}/bot/reset`, {
-            headers: this.headers
-        })
-        return reset.mfa.ticket as string
-    }
-
-    protected async getCookie(ticket: string, password: string = this.password) {
-        const cookie = await post(`/mfa/finish`, {
-            headers: this.headers,
-            body: JSON.stringify({
-                'data': password,
-                'mfa_type': 'password',
-                'ticket': ticket
-            })
-        })
-        return cookie.token as string
-    }
-
-    protected async getToken(id: string, cookie: string) {
-        const response = await post(`/applications/${id}/bot/reset`, {
-            headers: {
-                ...this.headers,
-                'x-discord-mfa-authorization': cookie
-            }
-        })
-        return response.token as string;
+    
+    protected async deleteApplication(id: string) {
+        if (typeof id !== 'string' || !id) throw Error('ID not valid!')
+        const response = await this.rest.deleteApplication(id);
+        return response
     }
 }
--- src/bot-manager.ts
+++ src/bot-manager.ts
@@ -1,24 +1,49 @@
 import { Application } from './application'
 
+interface Options {
+    name: string
+    description?: string
+    tags?: string[]
+    owners?: string[]
+    avatar?: Buffer
+}
+
 export class Bot extends Application {
     private name!: string
+    private description?: string
+    private tags?: string[]
+    private owners?: string[]
+    private avatar?: Buffer
 
-    constructor(options: BotOptions) {
+    constructor(options: Options) {
         super()
         this.validate(options)
     }
     
-    private validate(options: BotOptions) {
+    private validate(options: Options) {
         if (typeof options !== 'object') throw TypeError('Options should be a type of Object.')
-        if (!options.name) throw Error('Please specify the bot name!')
+        if (!options.name) throw Error('You must provide the name of the application.')
+        if (!Array.isArray(options.tags)) throw TypeError('Tag(s) should be a type of Array<String>')
+        if (!Array.isArray(options.owners)) throw TypeError('Owner(s) should be a type of Array<String>')
+        if (Buffer.isBuffer(options.avatar)) throw TypeError('Avatar should be a type of Buffer.')
         this.name = options.name
+        this.description = options.description
+        this.tags = options.tags
+        this.owners = options.owners
     }
 
-    public async create() {
-        const id = await this.getId(this.name)
-        const ticket = await this.getTicket(id, this.password)
-        const cookie = await this.getCookie(ticket, this.password)
-        const token = await this.getToken(id, cookie)
-        return token
+    async get(id: string) {
+        const application = await this.getApplication(id)
+        return application
+    }
+
+    async create() {
+        const application = await this.createApplication(this.name)
+        return application
+    }
+
+    async delete(id: string) {
+        const application = await this.deleteApplication(id)
+        return application
     }
 }
